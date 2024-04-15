04d5830faeac850473aebd630b12c9380cef81ee
AL1Skey <adamnurramadan@gmail.com>
2024-04-15 11:20:44 +0700

    first commit

---
+++ myApp.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+const helmet = require('helmet')
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
